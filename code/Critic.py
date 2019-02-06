da165bb1bcf27df45bd43afbffdcacb287c02532
Rory McGrath <rory.m.mc.grath@accenture.com>
2019-02-06 18:20:51 +0000

    Resolved architecture issues in Critic.

--- code/Critic.py
+++ code/Critic.py
@@ -22,7 +22,7 @@ class Critic(nn.Module):
 		super(Critic,self).__init__()
 		self.seed = torch.manual_seed(seed)
 		self.fc1 = nn.Linear(state_size,400)
-		self.fc2 = nn.Linear(400+action_size,300)
+		self.fc2 = nn.Linear(400,300)
 		self.fc3 = nn.Linear(300+action_size,200)
 		self.fc4 = nn.Linear(200,1)
 
@@ -50,7 +50,7 @@ class Critic(nn.Module):
 		Value : float
 			The estimate value of the value function for the current state action pair.
 		'''
-		x = F.relu(self.f2(F.relu(self.fc1(state))))
+		x = F.relu(self.fc2(F.relu(self.fc1(state))))
 		x1 = torch.cat((x,action.float()),dim=1)
 		return self.fc4(F.relu(self.fc3(x1)))
 
